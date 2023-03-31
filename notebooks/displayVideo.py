d60f25870935423a47be7dbc8bc484452d5f0492
dzs-liulinpeng <ddfightliu@hotmail.com>
2023-03-31 14:12:03 +0800

    20230401

--- notebooks/displayVideo.py
+++ notebooks/displayVideo.py
@@ -1,8 +1,8 @@
 from IPython.display import display, HTML
 
 
-def displayVideo():
-    html_str = '''
-            <video src=\"{}\" />
-        '''
+def displayVideo(path):
+    html_str = "<video src=\""+path+"\" ></video>"
+
+    print(html_str)
     display(HTML(html_str))
